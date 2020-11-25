13d3d649533f6ff637bf569e7c30cb7ff04ad62f
Alex Stein <devilarxgada@gmail.com>
2020-11-25 22:46:35 +0500

    Add basic code

--- SimpleWin32Debugger/Debugger.cpp
+++ SimpleWin32Debugger/Debugger.cpp
@@ -1,1 +1,9 @@
 #include "Debugger.h"
+
+std::string Debugger::_executableName = "";
+bool Debugger::_readyFlag = false;
+bool Debugger::_continue_flag = true;
+
+bool Debugger::init(std::string execName) {
+
+}
--- SimpleWin32Debugger/Debugger.h
+++ SimpleWin32Debugger/Debugger.h
@@ -1,5 +1,18 @@
 #pragma once
+
+#include <string>
+#include <optional>
+#include <Windows.h>
+
 class Debugger
 {
+public:
+	using DebugEventCode = DWORD;
+	static bool init(std::string execName);
+	static std::optional<DebugEventCode> nextDebugEvent();
+private:
+	static std::string _executableName;
+	static bool _readyFlag;
+	static bool _continue_flag;
 };
 
