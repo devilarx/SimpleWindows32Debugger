886138804e5c3db2702d1cf59b41daeecf86e559
Alex Stein <devilarxgada@gmail.com>
2020-11-26 10:52:31 +0500

    Add some code

--- SimpleWin32Debugger/Debugger.cpp
+++ SimpleWin32Debugger/Debugger.cpp
@@ -1,9 +1,42 @@
 #include "Debugger.h"
 
+using namespace sdb;
+
 std::string Debugger::_executableName = "";
 bool Debugger::_readyFlag = false;
 bool Debugger::_continue_flag = true;
+std::list<Breakpoint> dbList;
+HANDLE glDebugProcess = INVALID_HANDLE_VALUE;
 
 bool Debugger::init(std::string execName) {
+	STARTUPINFOA startupInfo;
+	PROCESS_INFORMATION procInfo;
 
+	RtlZeroMemory(&startupInfo, sizeof(startupInfo));
+	RtlZeroMemory(&procInfo, sizeof(procInfo));
+	startupInfo.cb = sizeof(startupInfo);
+	startupInfo.dwFlags = STARTF_USESHOWWINDOW;
+	startupInfo.wShowWindow = SW_SHOWNORMAL;
+
+	Debugger::_readyFlag = CreateProcessA(
+		execName.c_str(),
+		NULL,
+		NULL,
+		NULL,
+		TRUE,
+		DEBUG_ONLY_THIS_PROCESS,
+		NULL,
+		NULL,
+		&startupInfo,
+		&procInfo
+	);
+	if (Debugger::_readyFlag)
+		Debugger::_executableName = execName;
+	Debugger::glDebugProcess = procInfo.hProcess;
+	CloseHandle(procInfo.hThread);
+	return Debugger::_readyFlag;
+}
+
+std::optional<Debugger::DebugEventCode> Debugger::nextDebugEvent() {
+	return std::nullopt;
 }
--- SimpleWin32Debugger/Debugger.h
+++ SimpleWin32Debugger/Debugger.h
@@ -2,17 +2,33 @@
 
 #include <string>
 #include <optional>
+#include <list>
+
 #include <Windows.h>
 
-class Debugger
-{
-public:
-	using DebugEventCode = DWORD;
-	static bool init(std::string execName);
-	static std::optional<DebugEventCode> nextDebugEvent();
-private:
-	static std::string _executableName;
-	static bool _readyFlag;
-	static bool _continue_flag;
-};
+namespace sdb {
 
+	struct Breakpoint {
+		unsigned int state;
+		unsigned int type;
+		unsigned int addr;
+		unsigned int saveByte;
+	};
+
+	class Debugger
+	{
+	public:
+		using DebugEventCode = DWORD;
+		static bool init(std::string execName);
+		static std::optional<DebugEventCode> nextDebugEvent();
+	private:
+		static std::string _executableName;
+		static std::list<Breakpoint> bpList;
+
+		static bool _readyFlag;
+		static bool _continue_flag;
+
+		static HANDLE glDebugProcess;
+	};
+
+}
